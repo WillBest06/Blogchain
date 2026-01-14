f89f11008f75663ba602fc8add3aad0b49febd76
William Best <148398824+WillBest06@users.noreply.github.com>
2026-01-14 17:42:51 +0000

    add password hashing and user loading to model

--- app/models.py
+++ app/models.py
@@ -1,13 +1,26 @@
-from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from . import login_manager
 
 db = SQLAlchemy()
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.execute(select(User).where(User.id == int(user_id)))
+
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id            = db.Column(db.Integer, primary_key=True)
     username      = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(120), unique=False, nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     posts = db.relationship(
                 'Post',
@@ -41,7 +54,6 @@ class Post(db.Model):
                 'User',
                 back_populates='posts',
                 lazy=True,
-                cascade="all, delete-orphan"
             )
 
     def save(self):
