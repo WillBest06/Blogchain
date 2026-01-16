cd10f36c8c0fb14e442faffb7f8b1dacfda5967c
William Best <148398824+WillBest06@users.noreply.github.com>
2026-01-16 10:45:10 +0000

    add support for replies to reduce code duplication

--- app/routes/create.py
+++ app/routes/create.py
@@ -1,12 +1,17 @@
 from app.models import Post, db
-from flask import flash, redirect, Blueprint, render_template, url_for
+from flask import flash, redirect, Blueprint, render_template, url_for, request
 from flask_login import login_required, current_user
 
 create_bp = Blueprint("create", __name__)
 
-@create_bp.route('/newpost', methods=['GET', 'POST'])
+@create_bp.route('/post', methods=['GET', 'POST'])
 @login_required
 def create():
+    parent_id = request.args.get("reply_to")
+    parent_post = None
+    if parent_id:
+        parent_post = db.session.execute(db.select(Post).where(Post.id == parent_id)).scalar()
+    print(parent_post)
     from .forms.create.PostForm import PostForm
     # PostForm is a WTForms custom form
     form = PostForm()
@@ -15,11 +20,15 @@ def create():
         post.title = form.title.data
         post.body = form.body.data
         post.user_id = current_user.id
-        post.parent_id = None
+        post.parent_id = parent_id
 
         db.session.add(post)
         db.session.commit()
-        flash('Post created!')
+
+        if parent_id:
+            flash("Reply sent!")
+        else:
+            flash('Post created!')
         return redirect(url_for('home.home')) 
 
-    return render_template('create/createPost.html', form=form)
+    return render_template('create/post.html', form=form, parent=parent_post)
