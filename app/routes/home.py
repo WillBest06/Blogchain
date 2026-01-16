a1c614c65a45965e302f19575c3eeb623637fe1a
William Best <148398824+WillBest06@users.noreply.github.com>
2026-01-16 11:03:19 +0000

    rename stories to posts

--- app/routes/home.py
+++ app/routes/home.py
@@ -7,9 +7,9 @@ home_bp = Blueprint("home", __name__)
 @home_bp.route('/')
 def home():
     # query only gets posts without a parent
-    stories = db.session.execute(db.select(Post).where(Post.parent_id == None).order_by(Post.created.desc())).scalars().all()
+    posts = db.session.execute(db.select(Post).where(Post.parent_id == None).order_by(Post.created.desc())).scalars().all()
     
-    for story in stories:
-        author = db.session.execute(db.select(User).where(User.id == story.user_id)).scalar()
-        story.author = author.username
-    return render_template('home.html', current_user=current_user, stories=stories)
+    for post in posts:
+        author = db.session.execute(db.select(User).where(User.id == post.user_id)).scalar()
+        post.author = author.username
+    return render_template('home.html', current_user=current_user, posts=posts)
